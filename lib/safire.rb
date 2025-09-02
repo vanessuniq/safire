e304536bfb60a5d86f03c6216e9e3254a69e5bec
Vanessa Fotso <vanessuniq@gmail.com>
2025-09-02 15:16:23 -0400

    simple implementation of safire.rb
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

---
+++ lib/safire.rb
@@ -0,0 +1,26 @@
+# lib/safire.rb
+require_relative 'safire/version'
+
+# Main module for Safire gem
+module Safire
+  class << self
+    attr_accessor :configuration
+
+    def configure
+      @configuration ||=Configuration.new
+      yield(configuration)
+    end
+
+  end
+
+  class Configuration
+    attr_accessor :logger, :log_level, :timeout, :user_agent
+
+    def initialize
+      @logger = Logger.new($stdout)
+      @log_level = :info
+      @timeout = 30
+      @user_agent = "Safire v#{Safire::VERSION}"
+    end
+  end
+end

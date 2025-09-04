7c01c7556f0bef4e1946944fb128f2fc48508ef9
Vanessa Fotso <vanessuniq@gmail.com>
2025-09-04 15:04:54 -0400

    initial errors
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

---
+++ lib/safire/core/errors.rb
@@ -0,0 +1,29 @@
+module Safire
+  module Core
+    # Base error class for all Safire errors
+    class Error < StandardError
+      attr_reader :details
+
+      def initialize(message = nil, details: nil)
+        super(message)
+        @details = details
+      end
+    end
+
+    class ConfigurationError < Error; end
+
+    class AuthError < Error; end
+
+    class DiscoveryError < Error; end
+
+    class TokenError < Error; end
+
+    class CertificateError < Error; end
+
+    class ProtocolError < Error; end
+
+    class NetworkError < Error; end
+
+    class ValidationError < Error; end
+  end
+end

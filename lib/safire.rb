9fd0b535b47efd2dc414d2674ad6e4bf6b5d38f0
Vanessa Fotso <vanessuniq@gmail.com>
2025-10-20 14:19:53 -0400

    Update file loading paths and require necessary files in safire.rb
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- lib/safire.rb
+++ lib/safire.rb
@@ -1,7 +1,9 @@
 require 'active_support/all'
 
+require_relative 'safire/entity'
+
 root = File.expand_path '.', File.dirname(File.absolute_path(__FILE__))
-Dir.glob(File.join(root, 'safire', 'client', '**', '*.rb')).each do |file|
+Dir.glob(File.join(root, 'safire', 'protocols', '**', '*.rb')).each do |file|
   require file
 end
 
@@ -9,7 +11,8 @@ require_relative 'safire/version'
 require_relative 'safire/errors'
 require_relative 'safire/http_client'
 require_relative 'safire/safire_logger'
-require_relative 'safire/safire_client'
+require_relative 'safire/client_config'
+require_relative 'safire/client'
 
 # Main module for Safire gem
 module Safire

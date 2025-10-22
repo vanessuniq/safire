20369910f573a7dfdad903812d96929b31fb1bbb
Vanessa Fotso <vanessuniq@gmail.com>
2025-10-21 21:09:42 -0400

    Reorder require statements to account for dependencies order
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- lib/safire.rb
+++ lib/safire.rb
@@ -1,5 +1,9 @@
 require 'active_support/all'
 
+require_relative 'safire/version'
+require_relative 'safire/safire_logger'
+require_relative 'safire/errors'
+require_relative 'safire/http_client'
 require_relative 'safire/entity'
 
 root = File.expand_path '.', File.dirname(File.absolute_path(__FILE__))
@@ -7,10 +11,6 @@ Dir.glob(File.join(root, 'safire', 'protocols', '**', '*.rb')).each do |file|
   require file
 end
 
-require_relative 'safire/version'
-require_relative 'safire/errors'
-require_relative 'safire/http_client'
-require_relative 'safire/safire_logger'
 require_relative 'safire/client_config'
 require_relative 'safire/client'
 

bba7b01e46aa33f012632a9bba3cb36659c44970
Vanessa Fotso <vanessuniq@gmail.com>
2025-10-28 11:52:35 -0400

    Refactored the logic for smart metadata in the unified client and update docs
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- lib/safire/client.rb
+++ lib/safire/client.rb
@@ -1,6 +1,6 @@
 module Safire
   class Client
-    attr_reader :config, :smart_metadata
+    attr_reader :config
 
     # Initialie Safire client with a set of config
     #
@@ -9,10 +9,8 @@ module Safire
       @config = config
     end
 
-    def smart_discovery
-      return smart_metadata if smart_metadata
-
-      @smart_metadata = Safire::Protocols::Smart::Discovery.new(config.base_url).discover
+    def smart_metadata
+      @smart_metadata ||= Safire::Protocols::Smart::Discovery.new(config.base_url).discover
     end
   end
 end

2cb23714d5033e5bc5971e6f648e06fc1f745c8a
Vanessa Fotso <vanessuniq@gmail.com>
2025-10-28 23:12:41 -0400

    add method to convert entities objects to hash
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- lib/safire/entity.rb
+++ lib/safire/entity.rb
@@ -3,5 +3,14 @@ module Safire
     def initialize(params, attributes)
       attributes.each { |name| instance_variable_set("@#{name}", params[name] || params[name.to_s]) }
     end
+
+    def to_hash
+      hash = {}
+      instance_variables.each do |var|
+        key = var.to_s.delete_prefix('@').to_sym
+        hash[key] = instance_variable_get(var)
+      end
+      hash
+    end
   end
 end

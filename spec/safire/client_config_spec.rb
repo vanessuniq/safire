170469aa4cc4ccf77fc33400f312f5167bde8a07
Vanessa Fotso <vanessuniq@gmail.com>
2026-04-06 14:55:18 -0400

    spec: optional client_id and per-flow validation tests
    Signed-off-by: Vanessa Fotso <vanessuniq@gmail.com>

--- spec/safire/client_config_spec.rb
+++ spec/safire/client_config_spec.rb
@@ -20,11 +20,13 @@ RSpec.describe Safire::ClientConfig do
       expect { described_class.new(valid_attrs.except(:redirect_uri)) }.not_to raise_error
     end
 
-    %i[base_url client_id].each do |attr|
-      it "raises ConfigurationError when #{attr} is missing" do
-        expect { described_class.new(valid_attrs.except(attr)) }
-          .to raise_error(Safire::Errors::ConfigurationError, /#{attr}/)
-      end
+    it 'initializes successfully without client_id' do
+      expect { described_class.new(valid_attrs.except(:client_id)) }.not_to raise_error
+    end
+
+    it 'raises ConfigurationError when base_url is missing' do
+      expect { described_class.new(valid_attrs.except(:base_url)) }
+        .to raise_error(Safire::Errors::ConfigurationError, /base_url/)
     end
   end
 
